340bb9818531ef4fd767e901aaa669d896a22584
sthejeswani <sthejeswani@US-PG0337ER>
2022-09-26 22:47:47 +0530

    First Commit

---
+++ spring-boot-h2/src/test/java/com/javatechie/h2/api/SpringBootH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.h2.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
